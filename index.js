65b9590ea91115470db2b4a989d5813eb1d7228a
Vins <vinstech8@yahoo.com>
2018-03-03 23:09:53 -0500

    created a not-so-small index

---
+++ index.js
@@ -0,0 +1,33 @@
+const commando = require("discord.js-commando");
+const config = require("./config.json");
+const client = new commando.Client({
+    owner: config.ownerid,
+    commandPrefix = config.prefix
+});
+
+client.on("ready", () => { // Startup
+    console.log("Discord-chan is online.");
+    client.user.setActivity("with Vin's code | d!");
+});
+
+client.on("guildCreate", guild => { // Join a Guild 
+    console.log(`Discord-chan has joined ${guild.name}. ID: ${guild.id}`);
+});
+
+client.on("guildDelete", guild => { // Leave a Guild
+    console.log(`Discord-chan has left ${guild.name}. ID: ${guild.id}`);
+});
+
+client.on("message", () => { // Message sent
+
+});
+
+client.registry
+    .registerCommandsIn(`${__dirname}/commands`)
+    .registerDefaults()
+    .registerGroups([
+        ["basic", "Basic commands"],
+        ["management", "Management commands"]
+    ]);
+
+client.login(config.token);
