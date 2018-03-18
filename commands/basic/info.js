4d7c4a69c1bebe81e8a4b0cdfda433aae09e0b54
Vins <vinstech8@yahoo.com>
2018-03-17 23:28:10 -0400

    now you can stalk people in style

---
+++ commands/basic/info.js
@@ -0,0 +1,42 @@
+const commando = require("discord.js-commando");
+const config = require("../.././config.json");
+const { RichEmbed } = require("discord.js");
+
+class info extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: "info",
+            aliases: ["av", "person"],
+            group: "basic",
+            memberName: "info",
+            description: "Discord-chan pastes out information of a person specified.",
+            details: "Discord-chan pastes out various information of a person specified by a tag.",
+            format: "<@user>",
+            examples: ["d!info @Vins#9790", "d!info Vins"],
+            args: [{
+                key: "user",
+                prompt: "Which person would you like to pull information of?",
+                type: "user"
+            }]
+        });
+    }
+    async run(message, args) {
+        let embed = new RichEmbed()
+            .setAuthor(args.user.username, args.user.displayAvatarURL)
+            .setURL(args.user.displayAvatarURL)
+            .setColor(message.guild.member(args.user).highestRole.color)
+            .setImage(args.user.displayAvatarURL)
+            .setDescription(`[Direct Avatar Link!](${args.user.displayAvatarURL})`)
+            .addField("User ID", args.user.id, true)
+            .addField("Discriminator Tag", args.user.tag, true)
+            .addField("Status", args.user.presence.status.toUpperCase(), true)
+            .setTimestamp(args.user.createdAt)
+            .setFooter("Account created on");
+
+        if (args.user.presence.game) embed.addField("Activity", args.user.presence.game.name, true);
+
+        message.channel.send({embed});
+    }
+}
+
+module.exports = info;
